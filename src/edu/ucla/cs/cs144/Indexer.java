9f13e6553f464eb8ecf99457efb8f8c675927cc8
Byron Lutz <byronlutz@gmail.com>
2013-11-01 20:23:29 -0700

    Removed comments and made some setup code for getting our IndexWriter going

--- src/edu/ucla/cs/cs144/Indexer.java
+++ src/edu/ucla/cs/cs144/Indexer.java
@@ -30,8 +30,14 @@ public class Indexer {
         }
         return indexWriter;
     }
- 
-    public void rebuildIndexes() {
+
+    public void closeIndexWriter() throws IOException {
+        if (indexWriter != null) {
+            indexWriter.close();
+        }
+    }
+
+    public void rebuildIndexes() throws IOException {
 
         Connection conn = null;
 
@@ -42,26 +48,14 @@ public class Indexer {
 	    System.out.println(ex);
 	}
 
+        // Erase existing index
+        getIndexWriter(true);
 
-	/*
-	 * Add your code here to retrieve Items using the connection
-	 * and add corresponding entries to your Lucene inverted indexes.
-         *
-         * You will have to use JDBC API to retrieve MySQL data from Java.
-         * Read our tutorial on JDBC if you do not know how to use JDBC.
-         *
-         * You will also have to use Lucene IndexWriter and Document
-         * classes to create an index and populate it with Items data.
-         * Read our tutorial on Lucene as well if you don't know how.
-         *
-         * As part of this development, you may want to add
-         * new methods and create additional Java classes.
-         * If you create new classes, make sure that
-         * the classes become part of "edu.ucla.cs.cs144" package
-         * and place your class source files at src/edu/ucla/cs/cs144/.
-	 *
-	 */
+        // Index everthing we need to (as individual items)
+        // TODO
 
+        // Close the index writer
+        closeIndexWriter();
 
         // close the database connection
 	try {
@@ -71,7 +65,7 @@ public class Indexer {
 	}
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         Indexer idx = new Indexer();
         idx.rebuildIndexes();
     }
